57a2c1e3fee41e954469f3fd69d941d453851a15
Pierre Berthe <42767265+MrTsunami4@users.noreply.github.com>
2021-04-05 17:40:12 +0200

    Ajout add serveur

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,61 @@ use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use serenity::utils::MessageBuilder;
+use serenity::model::id::{GuildId, ChannelId};
+use serenity::model::guild::Member;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, context: Context, msg: Message) {
         if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+            let channel = match msg.channel_id.to_channel(&context).await {
+                Ok(channel) => channel,
+                Err(why) => {
+                    println!("Error getting channel: {:?}", why);
+
+                    return;
+                },
+            };
+
+            // The message builder allows for creating a message by
+            // mentioning users dynamically, pushing "safe" versions of
+            // content (such as bolding normalized content), displaying
+            // emojis, and more.
+            let response = MessageBuilder::new()
+                .push("User ")
+                .push_bold_safe(&msg.author.name)
+                .push(" used the 'ping' command in the ")
+                .mention(&channel)
+                .push(" channel")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        if msg.content == "!tt" {
+            if let Err(why) = msg.channel_id.say(&context.http, "test").await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
+    async fn guild_member_addition(&self, ctx:Context, guildid:GuildId, new_member: Member) {
+        let response = MessageBuilder::new()
+            .push("User ")
+            .push_bold_safe(&new_member.user.name)
+            .push(" used the 'coucou' command in the ")
+            .push(" channel")
+            .build();
+
+        if let Err(why) = ChannelId(827152172646531084).say(&ctx.http, &response).await {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
